1b368d669f3a55e061d6bdee02e5d442111a0cff
sandeepkhannavp <vpsandeepkhanna@gmail.com>
2023-01-27 04:44:05 +0530

    v2: changes-wallet

--- node/http_routes.go
+++ node/http_routes.go
@@ -1,52 +1,54 @@
 package node
 
 import (
-	"blockchain/database"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
 	"net/http"
+	"strconv"
 )
 
 type ErrRes struct {
-	Error string
+	Error string `json:"error"`
 }
 
 type BalancesRes struct {
-	Hash 	database.Hash
-	Balances map[database.Account]uint
+	Hash     database.Hash           `json:"block_hash"`
+	Balances map[common.Address]uint `json:"balances"`
 }
 
 type TxAddReq struct {
-	From string
-	To string
-	Value uint
-	Data string
+	From    string `json:"from"`
+	FromPwd string `json:"from_pwd"`
+	To      string `json:"to"`
+	Value   uint   `json:"value"`
+	Data    string `json:"data"`
 }
 
 type TxAddRes struct {
-	Hash database.Hash
+	Success bool `json:"success"`
 }
 
 type StatusRes struct {
-	Hash database.Hash
-	Number uint64
-	KnownPeers []PeerNode
+	Hash       database.Hash       `json:"block_hash"`
+	Number     uint64              `json:"block_number"`
+	KnownPeers map[string]PeerNode `json:"peers_known"`
+	PendingTXs []database.SignedTx `json:"pending_txs"`
 }
 
-
-func listBalancesHandler(w http.ResponseWriter,r *http.Request, state *database.State) {
-	writeRes(w,BalancesRes{state.LatestBlockHash(),state.Balances})
+type SyncRes struct {
+	Blocks []database.Block `json:"blocks"`
 }
 
-func statusHandler(w http.ResponseWriter, r* http.Request, n *Node) {
-	res := StatusRes {
-		Hash : n.state.LatestBlockHash(),
-		Number: n.state.LatestBlock().Header.Number,
-		KnownPeers: n.knownPeers,
-	}
-
-	writeRes(w, res)
+type AddPeerRes struct {
+	Success bool   `json:"success"`
+	Error   string `json:"error"`
 }
 
-func txAddHandler(w http.ResponseWriter, r* http.Request, state *database.State) {
+func listBalancesHandler(w http.ResponseWriter, r *http.Request, state *database.State) {
+	writeRes(w, BalancesRes{state.LatestBlockHash(), state.Balances})
+}
+
+func txAddHandler(w http.ResponseWriter, r *http.Request, node *Node) {
 	req := TxAddReq{}
 	err := readReq(r, &req)
 	if err != nil {
@@ -54,19 +56,82 @@ func txAddHandler(w http.ResponseWriter, r* http.Request, state *database.State)
 		return
 	}
 
-	tx := database.NewTx(database.NewAccount(req.From), database.NewAccount(req.To), req.Value, req.Data)
+	from := database.NewAccount(req.From)
 
-	err = state.AddTx(tx)
+	if from.String() == common.HexToAddress("").String() {
+		writeErrRes(w, fmt.Errorf("%s is an invalid 'from' sender", from.String()))
+		return
+	}
+
+	if req.FromPwd == "" {
+		writeErrRes(w, fmt.Errorf("password to decrypt the %s account is required. 'from_pwd' is empty", from.String()))
+		return
+	}
+
+	nonce := node.state.GetNextAccountNonce(from)
+	tx := database.NewTx(from, database.NewAccount(req.To), req.Value, nonce, req.Data)
+
+	signedTx, err := wallet.SignTxWithKeystoreAccount(tx, from, req.FromPwd, wallet.GetKeystoreDirPath(node.dataDir))
 	if err != nil {
 		writeErrRes(w, err)
 		return
 	}
 
-	hash, err := state.Persist()
+	err = node.AddPendingTX(signedTx, node.info)
 	if err != nil {
 		writeErrRes(w, err)
 		return
 	}
-	
-	writeRes(w, TxAddRes{hash})
+
+	writeRes(w, TxAddRes{Success: true})
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request, node *Node) {
+	res := StatusRes{
+		Hash:       node.state.LatestBlockHash(),
+		Number:     node.state.LatestBlock().Header.Number,
+		KnownPeers: node.knownPeers,
+		PendingTXs: node.getPendingTXsAsArray(),
+	}
+
+	writeRes(w, res)
+}
+
+func syncHandler(w http.ResponseWriter, r *http.Request, node *Node) {
+	reqHash := r.URL.Query().Get(endpointSyncQueryKeyFromBlock)
+
+	hash := database.Hash{}
+	err := hash.UnmarshalText([]byte(reqHash))
+	if err != nil {
+		writeErrRes(w, err)
+		return
+	}
+
+	blocks, err := database.GetBlocksAfter(hash, node.dataDir)
+	if err != nil {
+		writeErrRes(w, err)
+		return
+	}
+
+	writeRes(w, SyncRes{Blocks: blocks})
+}
+
+func addPeerHandler(w http.ResponseWriter, r *http.Request, node *Node) {
+	peerIP := r.URL.Query().Get(endpointAddPeerQueryKeyIP)
+	peerPortRaw := r.URL.Query().Get(endpointAddPeerQueryKeyPort)
+	minerRaw := r.URL.Query().Get(endpointAddPeerQueryKeyMiner)
+
+	peerPort, err := strconv.ParseUint(peerPortRaw, 10, 32)
+	if err != nil {
+		writeRes(w, AddPeerRes{false, err.Error()})
+		return
+	}
+
+	peer := NewPeerNode(peerIP, peerPort, false, database.NewAccount(minerRaw), true)
+
+	node.AddPeer(peer)
+
+	fmt.Printf("Peer '%s' was added into KnownPeers\n", peer.TcpAddress())
+
+	writeRes(w, AddPeerRes{true, ""})
 }
--- node/node.go
+++ node/node.go
@@ -1,63 +1,266 @@
 package node
 
 import (
-	"blockchain/database"
+	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/ethereum/go-ethereum/common"
 	"net/http"
+	"time"
 )
 
-const DefaultHttpPort = 8080
+const DefaultBootstrapIp = "node.tbb.web3.coach"
+const DefaultBootstrapPort = 8080
+const DefaultBootstrapAcc = wallet.AndrejAccount
+const DefaultMiner = "0x0000000000000000000000000000000000000000"
+const DefaultIP = "127.0.0.1"
+const DefaultHTTPort = 8080
+const endpointStatus = "/node/status"
+
+const endpointSync = "/node/sync"
+const endpointSyncQueryKeyFromBlock = "fromBlock"
+
+const endpointAddPeer = "/node/peer"
+const endpointAddPeerQueryKeyIP = "ip"
+const endpointAddPeerQueryKeyPort = "port"
+const endpointAddPeerQueryKeyMiner = "miner"
+
+const miningIntervalSeconds = 10
 
 type PeerNode struct {
-	IP string
-	Port uint64
-	IsBootstrap bool
-	IsActive bool
+	IP          string         `json:"ip"`
+	Port        uint64         `json:"port"`
+	IsBootstrap bool           `json:"is_bootstrap"`
+	Account     common.Address `json:"account"`
+
+	// Whenever my node already established connection, sync with this Peer
+	connected bool
+}
+
+func (pn PeerNode) TcpAddress() string {
+	return fmt.Sprintf("%s:%d", pn.IP, pn.Port)
 }
 
 type Node struct {
 	dataDir string
-	port uint64
-	state *database.State
-	knownPeers []PeerNode
+	info    PeerNode
+
+	state           *database.State
+	knownPeers      map[string]PeerNode
+	pendingTXs      map[string]database.SignedTx
+	archivedTXs     map[string]database.SignedTx
+	newSyncedBlocks chan database.Block
+	newPendingTXs   chan database.SignedTx
+	isMining        bool
 }
 
-func New(dataDir string, port uint64, bootstrap PeerNode) *Node {
-	return &Node {
-		dataDir: dataDir,
-		port: port,
-		knownPeers: []PeerNode{bootstrap},
+func New(dataDir string, ip string, port uint64, acc common.Address, bootstrap PeerNode) *Node {
+	knownPeers := make(map[string]PeerNode)
+	knownPeers[bootstrap.TcpAddress()] = bootstrap
+
+	return &Node{
+		dataDir:         dataDir,
+		info:            NewPeerNode(ip, port, false, acc, true),
+		knownPeers:      knownPeers,
+		pendingTXs:      make(map[string]database.SignedTx),
+		archivedTXs:     make(map[string]database.SignedTx),
+		newSyncedBlocks: make(chan database.Block),
+		newPendingTXs:   make(chan database.SignedTx, 10000),
+		isMining:        false,
 	}
 }
 
-func NewPeerNode(ip string, port uint64, isBootstrap bool, isActive bool) PeerNode {
-	return PeerNode{ip, port, isBootstrap,isActive}
+func NewPeerNode(ip string, port uint64, isBootstrap bool, acc common.Address, connected bool) PeerNode {
+	return PeerNode{ip, port, isBootstrap, acc, connected}
 }
 
-func (n *Node)Run() error {
-	fmt.Printf("Listening on port: %d\n",n.port)
+func (n *Node) Run(ctx context.Context) error {
+	fmt.Println(fmt.Sprintf("Listening on: %s:%d", n.info.IP, n.info.Port))
 
 	state, err := database.NewStateFromDisk(n.dataDir)
-	
 	if err != nil {
 		return err
 	}
-
 	defer state.Close()
 
 	n.state = state
 
-	http.HandleFunc("/balances/list",func(w http.ResponseWriter, r *http.Request){
-		listBalancesHandler(w,r,state)
+	fmt.Println("Blockchain state:")
+	fmt.Printf("	- height: %d\n", n.state.LatestBlock().Header.Number)
+	fmt.Printf("	- hash: %s\n", n.state.LatestBlockHash().Hex())
+
+	go n.sync(ctx)
+	go n.mine(ctx)
+
+	handler := http.NewServeMux()
+
+	handler.HandleFunc("/balances/list", func(w http.ResponseWriter, r *http.Request) {
+		listBalancesHandler(w, r, state)
 	})
 
-	http.HandleFunc("/tx/add",func(w http.ResponseWriter, r*http.Request){
-		txAddHandler(w,r,state)
+	handler.HandleFunc("/tx/add", func(w http.ResponseWriter, r *http.Request) {
+		txAddHandler(w, r, n)
 	})
 
-	http.HandleFunc("/node/status",func(w http.ResponseWriter, r *http.Request) {
-		statusHandler(w,r,n)
+	handler.HandleFunc(endpointStatus, func(w http.ResponseWriter, r *http.Request) {
+		statusHandler(w, r, n)
 	})
 
-	return http.ListenAndServe(fmt.Sprintf(":%d",n.port),nil)
+	handler.HandleFunc(endpointSync, func(w http.ResponseWriter, r *http.Request) {
+		syncHandler(w, r, n)
+	})
+
+	handler.HandleFunc(endpointAddPeer, func(w http.ResponseWriter, r *http.Request) {
+		addPeerHandler(w, r, n)
+	})
+
+	server := &http.Server{Addr: fmt.Sprintf(":%d", n.info.Port), Handler: handler}
+
+	go func() {
+		<-ctx.Done()
+		_ = server.Close()
+	}()
+
+	err = server.ListenAndServe()
+	// This shouldn't be an error!
+	if err != http.ErrServerClosed {
+		return err
+	}
+
+	return nil
+}
+
+func (n *Node) LatestBlockHash() database.Hash {
+	return n.state.LatestBlockHash()
+}
+
+func (n *Node) mine(ctx context.Context) error {
+	var miningCtx context.Context
+	var stopCurrentMining context.CancelFunc
+
+	ticker := time.NewTicker(time.Second * miningIntervalSeconds)
+
+	for {
+		select {
+		case <-ticker.C:
+			go func() {
+				if len(n.pendingTXs) > 0 && !n.isMining {
+					n.isMining = true
+
+					miningCtx, stopCurrentMining = context.WithCancel(ctx)
+					err := n.minePendingTXs(miningCtx)
+					if err != nil {
+						fmt.Printf("ERROR: %s\n", err)
+					}
+
+					n.isMining = false
+				}
+			}()
+
+		case block, _ := <-n.newSyncedBlocks:
+			if n.isMining {
+				blockHash, _ := block.Hash()
+				fmt.Printf("\nPeer mined next Block '%s' faster :(\n", blockHash.Hex())
+
+				n.removeMinedPendingTXs(block)
+				stopCurrentMining()
+			}
+
+		case <-ctx.Done():
+			ticker.Stop()
+			return nil
+		}
+	}
+}
+
+func (n *Node) minePendingTXs(ctx context.Context) error {
+	blockToMine := NewPendingBlock(
+		n.state.LatestBlockHash(),
+		n.state.NextBlockNumber(),
+		n.info.Account,
+		n.getPendingTXsAsArray(),
+	)
+
+	minedBlock, err := Mine(ctx, blockToMine)
+	if err != nil {
+		return err
+	}
+
+	n.removeMinedPendingTXs(minedBlock)
+
+	_, err = n.state.AddBlock(minedBlock)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (n *Node) removeMinedPendingTXs(block database.Block) {
+	if len(block.TXs) > 0 && len(n.pendingTXs) > 0 {
+		fmt.Println("Updating in-memory Pending TXs Pool:")
+	}
+
+	for _, tx := range block.TXs {
+		txHash, _ := tx.Hash()
+		if _, exists := n.pendingTXs[txHash.Hex()]; exists {
+			fmt.Printf("\t-archiving mined TX: %s\n", txHash.Hex())
+
+			n.archivedTXs[txHash.Hex()] = tx
+			delete(n.pendingTXs, txHash.Hex())
+		}
+	}
+}
+
+func (n *Node) AddPeer(peer PeerNode) {
+	n.knownPeers[peer.TcpAddress()] = peer
+}
+
+func (n *Node) RemovePeer(peer PeerNode) {
+	delete(n.knownPeers, peer.TcpAddress())
+}
+
+func (n *Node) IsKnownPeer(peer PeerNode) bool {
+	if peer.IP == n.info.IP && peer.Port == n.info.Port {
+		return true
+	}
+
+	_, isKnownPeer := n.knownPeers[peer.TcpAddress()]
+
+	return isKnownPeer
+}
+
+func (n *Node) AddPendingTX(tx database.SignedTx, fromPeer PeerNode) error {
+	txHash, err := tx.Hash()
+	if err != nil {
+		return err
+	}
+
+	txJson, err := json.Marshal(tx)
+	if err != nil {
+		return err
+	}
+
+	_, isAlreadyPending := n.pendingTXs[txHash.Hex()]
+	_, isArchived := n.archivedTXs[txHash.Hex()]
+
+	if !isAlreadyPending && !isArchived {
+		fmt.Printf("Added Pending TX %s from Peer %s\n", txJson, fromPeer.TcpAddress())
+		n.pendingTXs[txHash.Hex()] = tx
+		n.newPendingTXs <- tx
+	}
+
+	return nil
+}
+
+func (n *Node) getPendingTXsAsArray() []database.SignedTx {
+	txs := make([]database.SignedTx, len(n.pendingTXs))
+
+	i := 0
+	for _, tx := range n.pendingTXs {
+		txs[i] = tx
+		i++
+	}
+
+	return txs
 }
