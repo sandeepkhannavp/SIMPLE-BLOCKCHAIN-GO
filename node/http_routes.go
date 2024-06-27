e7ecf86859cd81258db8f50f520a84afb076b14d
sandeepkhannavp <vpsandeepkhanna@gmail.com>
2024-06-27 20:53:42 +0530

    code changes - reverting to old code to fix bugs

--- node/http_routes.go
+++ node/http_routes.go
@@ -1,54 +1,52 @@
 package node
 
 import (
-	"fmt"
-	"github.com/ethereum/go-ethereum/common"
+	"blockchain/database"
 	"net/http"
-	"strconv"
 )
 
 type ErrRes struct {
-	Error string `json:"error"`
+	Error string
 }
 
 type BalancesRes struct {
-	Hash     database.Hash           `json:"block_hash"`
-	Balances map[common.Address]uint `json:"balances"`
+	Hash 	database.Hash
+	Balances map[database.Account]uint
 }
 
 type TxAddReq struct {
-	From    string `json:"from"`
-	FromPwd string `json:"from_pwd"`
-	To      string `json:"to"`
-	Value   uint   `json:"value"`
-	Data    string `json:"data"`
+	From string
+	To string
+	Value uint
+	Data string
 }
 
 type TxAddRes struct {
-	Success bool `json:"success"`
+	Hash database.Hash
 }
 
 type StatusRes struct {
-	Hash       database.Hash       `json:"block_hash"`
-	Number     uint64              `json:"block_number"`
-	KnownPeers map[string]PeerNode `json:"peers_known"`
-	PendingTXs []database.SignedTx `json:"pending_txs"`
+	Hash database.Hash
+	Number uint64
+	KnownPeers []PeerNode
 }
 
-type SyncRes struct {
-	Blocks []database.Block `json:"blocks"`
+
+func listBalancesHandler(w http.ResponseWriter,r *http.Request, state *database.State) {
+	writeRes(w,BalancesRes{state.LatestBlockHash(),state.Balances})
 }
 
-type AddPeerRes struct {
-	Success bool   `json:"success"`
-	Error   string `json:"error"`
+func statusHandler(w http.ResponseWriter, r* http.Request, n *Node) {
+	res := StatusRes {
+		Hash : n.state.LatestBlockHash(),
+		Number: n.state.LatestBlock().Header.Number,
+		KnownPeers: n.knownPeers,
+	}
+
+	writeRes(w, res)
 }
 
-func listBalancesHandler(w http.ResponseWriter, r *http.Request, state *database.State) {
-	writeRes(w, BalancesRes{state.LatestBlockHash(), state.Balances})
-}
-
-func txAddHandler(w http.ResponseWriter, r *http.Request, node *Node) {
+func txAddHandler(w http.ResponseWriter, r* http.Request, state *database.State) {
 	req := TxAddReq{}
 	err := readReq(r, &req)
 	if err != nil {
@@ -56,82 +54,19 @@ func txAddHandler(w http.ResponseWriter, r *http.Request, node *Node) {
 		return
 	}
 
-	from := database.NewAccount(req.From)
+	tx := database.NewTx(database.NewAccount(req.From), database.NewAccount(req.To), req.Value, req.Data)
 
-	if from.String() == common.HexToAddress("").String() {
-		writeErrRes(w, fmt.Errorf("%s is an invalid 'from' sender", from.String()))
-		return
-	}
-
-	if req.FromPwd == "" {
-		writeErrRes(w, fmt.Errorf("password to decrypt the %s account is required. 'from_pwd' is empty", from.String()))
-		return
-	}
-
-	nonce := node.state.GetNextAccountNonce(from)
-	tx := database.NewTx(from, database.NewAccount(req.To), req.Value, nonce, req.Data)
-
-	signedTx, err := wallet.SignTxWithKeystoreAccount(tx, from, req.FromPwd, wallet.GetKeystoreDirPath(node.dataDir))
+	err = state.AddTx(tx)
 	if err != nil {
 		writeErrRes(w, err)
 		return
 	}
 
-	err = node.AddPendingTX(signedTx, node.info)
+	hash, err := state.Persist()
 	if err != nil {
 		writeErrRes(w, err)
 		return
 	}
-
-	writeRes(w, TxAddRes{Success: true})
-}
-
-func statusHandler(w http.ResponseWriter, r *http.Request, node *Node) {
-	res := StatusRes{
-		Hash:       node.state.LatestBlockHash(),
-		Number:     node.state.LatestBlock().Header.Number,
-		KnownPeers: node.knownPeers,
-		PendingTXs: node.getPendingTXsAsArray(),
-	}
-
-	writeRes(w, res)
-}
-
-func syncHandler(w http.ResponseWriter, r *http.Request, node *Node) {
-	reqHash := r.URL.Query().Get(endpointSyncQueryKeyFromBlock)
-
-	hash := database.Hash{}
-	err := hash.UnmarshalText([]byte(reqHash))
-	if err != nil {
-		writeErrRes(w, err)
-		return
-	}
-
-	blocks, err := database.GetBlocksAfter(hash, node.dataDir)
-	if err != nil {
-		writeErrRes(w, err)
-		return
-	}
-
-	writeRes(w, SyncRes{Blocks: blocks})
-}
-
-func addPeerHandler(w http.ResponseWriter, r *http.Request, node *Node) {
-	peerIP := r.URL.Query().Get(endpointAddPeerQueryKeyIP)
-	peerPortRaw := r.URL.Query().Get(endpointAddPeerQueryKeyPort)
-	minerRaw := r.URL.Query().Get(endpointAddPeerQueryKeyMiner)
-
-	peerPort, err := strconv.ParseUint(peerPortRaw, 10, 32)
-	if err != nil {
-		writeRes(w, AddPeerRes{false, err.Error()})
-		return
-	}
-
-	peer := NewPeerNode(peerIP, peerPort, false, database.NewAccount(minerRaw), true)
-
-	node.AddPeer(peer)
-
-	fmt.Printf("Peer '%s' was added into KnownPeers\n", peer.TcpAddress())
-
-	writeRes(w, AddPeerRes{true, ""})
+	
+	writeRes(w, TxAddRes{hash})
 }
